b282b6a1400948598f7baa77c65aa96c34b91ce5
frdmu <2320899425@qq.com>
2021-04-01 09:03:51 -0400

    simple & hashtable and switch

--- string/13_Roman_to_Integer.cc
+++ string/13_Roman_to_Integer.cc
@@ -56,3 +56,37 @@ public:
         return res;
     }
 };
+// solution 2
+// hashtable -> switch
+class Solution {
+public:
+    int romanToInt(string s) {
+        int ans = 0;
+        int len = s.size();
+
+        for (int i = 0; i < len-1; i++) {
+            int curVal = hash(s[i]);
+            int nextVal = hash(s[i+1]);
+            if (curVal >= nextVal) {
+                ans += curVal;
+            } else {
+                ans -= curVal;
+            }
+        }
+        ans += hash(s[len-1]);
+
+        return ans;
+    }
+    int hash(char ch) {
+        switch(ch) {
+            case 'I':   return 1;
+            case 'V':   return 5;
+            case 'X':   return 10;
+            case 'L':   return 50;
+            case 'C':   return 100;
+            case 'D':   return 500;
+            case 'M':   return 1000;
+        }
+        return 0;
+    }
+};
