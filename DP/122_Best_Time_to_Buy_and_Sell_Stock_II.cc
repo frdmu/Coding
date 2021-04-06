d66165eba161a771c2b306c7023376a5cf437780
frdmu <2320899425@qq.com>
2021-04-06 11:04:38 -0400

    simple

---
+++ DP/122_Best_Time_to_Buy_and_Sell_Stock_II.cc
@@ -0,0 +1,17 @@
+// https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int> > dp(n, vector<int>(2, 0));
+
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i]);
+            dp[i][1] = max(dp[i-1][0] - prices[i], dp[i-1][1]);
+        }
+
+        return dp[n-1][0];
+    }
+};
