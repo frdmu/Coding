c1c83204dc275420c39dc0fd02c8fb5d8c7a3197
frdmu <2320899425@qq.com>
2021-04-27 10:58:54 -0400

    simple & tree traversal

--- Tree/938_Range_Sum_of_BST.cc
+++ Tree/938_Range_Sum_of_BST.cc
@@ -22,3 +22,33 @@ public:
         return root->val + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
     }
 };
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void treeTraversal(TreeNode* root, int low, int high, int &sum) {
+        if (root == nullptr)
+            return;
+        if (root->val >= low && root->val <= high)
+            sum += root->val;
+        treeTraversal(root->left, low, high, sum);
+        treeTraversal(root->right, low, high, sum);
+    }
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        int sum = 0;
+        treeTraversal(root, low, high, sum);
+        return sum;
+    }
+};
