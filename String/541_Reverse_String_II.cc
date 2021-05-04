eec600ecc18f1b14e47cb42940ef70243369b278
frdmu <2320899425@qq.com>
2021-05-04 04:54:06 -0400

    simple & string & for step

---
+++ String/541_Reverse_String_II.cc
@@ -0,0 +1,18 @@
+// https://leetcode-cn.com/problems/reverse-string-ii/
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        int n = s.length();
+        
+        for (int i = 0; i < n; i += 2*k) {
+            if (i+k <= n) {
+                reverse(s.begin()+i, s.begin()+i+k);
+            } else {
+                reverse(s.begin()+i, s.end());
+            }
+
+        }
+
+        return s;
+    }
+};
