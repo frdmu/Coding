81ee355b9d73dc9149eff1348a9b57aa3696ae11
frdmu <2320899425@qq.com>
2021-05-07 10:34:10 -0400

    stack

--- Stack/20_Valid_Parentheses.cc
+++ Stack/20_Valid_Parentheses.cc
@@ -32,3 +32,25 @@ public:
         }
     }
 };
+// There are three cases that return false:
+// case 1: There are more parenthesis on the left such as ([]{}
+// case 2: There are more parenthesis on the right such as []{}}
+// case 3: The number of parentheses meets the criteria, but does not match such as ([)]
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        
+        for (auto ch : s) {
+            if (ch == '(') stk.push(')');
+            else if (ch == '[') stk.push(']');
+            else if (ch == '{') stk.push('}');
+            // case 2 and case 3
+            else if (stk.empty() || stk.top() != ch) return false;
+            else stk.pop();
+        }
+
+        // case 1
+        return stk.empty();
+    }
+};
