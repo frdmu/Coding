f8cba985aa39aeb960bc78e9e6e464327350ce4e
frdmu <2320899425@qq.com>
2021-05-07 09:26:27 -0400

    simple & two stacks implement queue

---
+++ Design/232_Implement_Queue_using_Stacks.cc
@@ -0,0 +1,50 @@
+// https://leetcode-cn.com/problems/implement-queue-using-stacks/
+// solution: input stack and output stack
+class MyQueue {
+public:
+    stack<int> stIn;
+    stack<int> stOut;
+    /** Initialize your data structure here. */
+    MyQueue() {
+
+    }
+    
+    /** Push element x to the back of queue. */
+    void push(int x) {
+        stIn.push(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+        if (stOut.empty()) {
+            while (!stIn.empty()) {
+                stOut.push(stIn.top());
+                stIn.pop();
+            }
+        }
+        int res = stOut.top();
+        stOut.pop();
+        return res;
+    }
+    
+    /** Get the front element. */
+    int peek() {
+        int res = this->pop();
+        stOut.push(res);
+        return res;
+    }
+    
+    /** Returns whether the queue is empty. */
+    bool empty() {
+        return stIn.empty() && stOut.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
