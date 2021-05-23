78fa52026020eb914e63a20aa59ffd58c912ab49
frdmu <2320899425@qq.com>
2021-05-24 01:04:02 +0800

    update

--- Backtracing/39_Combination_Sum.cc
+++ Backtracing/39_Combination_Sum.cc
@@ -26,3 +26,26 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    vector<int> path;
+    vector<vector<int> > ans;
+    void backtracing(vector<int> candidates, int target, int sum, int startIndex) {
+        if (sum == target) {
+            ans.push_back(path);
+            return;
+        } 
+        
+        for (int i = startIndex; i < candidates.size() && sum+candidates[i] <= target; i++) {
+            path.push_back(candidates[i]);
+            backtracing(candidates, target, sum+candidates[i], i);
+            path.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end()); 
+        backtracing(candidates, target, 0, 0);
+        return ans;         
+    }
+};
