a9168f011f3462984d94e1be7cf2dd962107b3b2
frdmu <2320899425@qq.com>
2021-05-23 23:33:11 +0800

    pruning

--- Backtracing/77_Combinations.cc
+++ Backtracing/77_Combinations.cc
@@ -19,3 +19,24 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    vector<vector<int> > ans;
+    vector<int> path;
+    void backtracing(int n, int k, int startIndex) {
+        if (path.size() == k) {
+            ans.push_back(path);
+            return;
+        }
+        for (int i = startIndex; i <= n-(k-path.size())+1; i++) {
+            path.push_back(i);
+            backtracing(n, k, i+1);
+            path.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        backtracing(n, k, 1);
+        return ans;
+    }
+};
