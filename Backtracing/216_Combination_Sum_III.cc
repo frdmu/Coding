f1c2000e25b449f8b8f968eef9bcbf3a34b960cf
frdmu <2320899425@qq.com>
2021-05-23 23:58:45 +0800

    pruning

--- Backtracing/216_Combination_Sum_III.cc
+++ Backtracing/216_Combination_Sum_III.cc
@@ -19,3 +19,27 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    vector<vector<int> > ans;
+    vector<int> path;
+    void backtracing(int targetSum, int k, int sum, int startIndex) {
+        if (sum > targetSum) {
+            return;
+        }
+        if (path.size() == k && sum == targetSum) {
+            ans.push_back(path);
+            return;
+        }
+        for (int i = startIndex; i <= 9-(k-path.size())+1; i++) {
+            path.push_back(i);
+            backtracing(targetSum, k, sum+i, i+1);
+            path.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        backtracing(n, k, 0, 1);
+        return ans;
+    }
+};
