259ce51001435f694b9228b1b21ff0e018e8d52a
frdmu <2320899425@qq.com>
2021-05-24 04:47:02 -0400

    update

--- Backtracing/40_Combination_Sum_II.cc
+++ Backtracing/40_Combination_Sum_II.cc
@@ -28,3 +28,29 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    vector<int> path;
+    vector<vector<int> > ans;
+    void backtracing(vector<int> candidates, int target, int sum, int startIndex, vector<bool>& visited) {
+        if (sum == target) {
+            ans.push_back(path);
+            return;
+        }
+        for (int i = startIndex; i < candidates.size() && sum+candidates[i] <= target; i++) {
+            if (i > 0 && candidates[i] == candidates[i-1] && visited[i-1] == false) continue;
+            visited[i] = 1;
+            path.push_back(candidates[i]);
+            backtracing(candidates, target, sum+candidates[i], i+1, visited);
+            path.pop_back();
+            visited[i] = 0;
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<bool> visited(candidates.size(), false);
+        sort(candidates.begin(), candidates.end());
+        backtracing(candidates, target, 0, 0, visited);
+        return ans;
+    }
+};
