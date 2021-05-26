1ebda20d52326dbe8fe0c5eb1643e9d45e168644
frdmu <2320899425@qq.com>
2021-05-25 23:36:40 -0400

    medium & duplicate

---
+++ Backtracing/491_Increasing_Subsequences.cc
@@ -0,0 +1,26 @@
+// https://leetcode-cn.com/problems/increasing-subsequences/
+class Solution {
+public:
+    vector<int> path;
+    vector<vector<int> > ans;
+    void backtracing(vector<int> nums, int startIndex) {
+        if (path.size() > 1) {
+            ans.push_back(path);
+        }
+
+        unordered_set<int> uset;
+        for (int i = startIndex; i < nums.size(); i++) {
+            if ((!path.empty() && nums[i] < path.back()) || uset.find(nums[i]) != uset.end()) {
+                continue;    
+            }
+            uset.insert(nums[i]);
+            path.push_back(nums[i]);
+            backtracing(nums, i+1);
+            path.pop_back();
+        }
+    }
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        backtracing(nums, 0);
+        return ans;
+    }
+};
