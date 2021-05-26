e6f7772d7475824119c9a16bf25bee34346f2a14
frdmu <2320899425@qq.com>
2021-05-25 23:42:52 -0400

    use array rather than set to increase performance

--- Backtracing/491_Increasing_Subsequences.cc
+++ Backtracing/491_Increasing_Subsequences.cc
@@ -24,3 +24,29 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    vector<int> path;
+    vector<vector<int> > ans;
+    void backtracing(vector<int> nums, int startIndex) {
+        if (path.size() > 1) {
+            ans.push_back(path);
+        }
+
+        int used[201] = {0};
+        for (int i = startIndex; i < nums.size(); i++) {
+            if ((!path.empty() && nums[i] < path.back()) || used[nums[i]+100] != 0) {
+                continue;
+            }
+            used[nums[i]+100] = 1;
+            path.push_back(nums[i]);
+            backtracing(nums, i+1);
+            path.pop_back();
+        }
+    }
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        backtracing(nums, 0);
+        return ans;
+    }
+};
