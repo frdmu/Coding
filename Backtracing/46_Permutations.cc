8d8b8ac63e77274685c70f819deafcf7a7543bf3
frdmu <2320899425@qq.com>
2021-05-27 03:08:03 -0400

    update

--- Backtracing/46_Permutations.cc
+++ Backtracing/46_Permutations.cc
@@ -33,3 +33,28 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    vector<int> path;
+    vector<vector<int> > ans;
+    void backtracing(vector<int>& nums, vector<bool>& used) {
+        if (path.size() == nums.size()) {
+            ans.push_back(path);
+            return;
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            if (used[i]) continue;
+            used[i] = true;
+            path.push_back(nums[i]);
+            backtracing(nums, used);
+            path.pop_back();
+            used[i] = false;
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<bool> used(nums.size(), false);
+        backtracing(nums, used);
+        return ans;
+    }
+};
