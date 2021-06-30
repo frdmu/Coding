3d5af4327922ed5dc6b6ebba596438a90ee38eea
frdmu <2320899425@qq.com>
2021-06-30 10:20:33 -0400

    medium & greedy

--- Greedy_algorithm/134_Gas_Station.cc
+++ Greedy_algorithm/134_Gas_Station.cc
@@ -26,3 +26,21 @@ public:
         return -1;
     }
 };
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size();
+        int cur = 0, total = 0, start = 0;
+        for (int i = 0; i < n; i++) {
+            cur += gas[i] - cost[i];
+            total += gas[i] - cost[i];
+            if (cur < 0) {
+                cur = 0;
+                start = i+1;
+            }
+        }
+        if (total >= 0) return start;
+        return -1;
+    }
+};
